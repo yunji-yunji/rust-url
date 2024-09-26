60214bfb5b9cb9de532094c9f8467ee9b9dda425
Yunji Kim <yj1230k@gmail.com>
2024-09-25 20:18:01 -0400

    update idna fuzz_target

--- idna/tests/idna_fuzz.rs
+++ idna/tests/idna_fuzz.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 use idna::*;
-use crate::punycode::encode_into;
+use idna::punycode::{encode_into, decode, decode_to_string, encode_str};
 
 #[test]
 fn quick_test() {
@@ -22,39 +22,49 @@ fn quick_test() {
 }
 
 fn run(data: &str) -> Result<(), Box<dyn Error>> {
+    println!("original input: {}", data);
+
     let data_byte = data.as_bytes();
+
+    // test 1
     let mut new_data_bytes = Vec::new();
     for d in data_byte.iter() {
         let invalid_data = if *d <= 35 { d + 36 } else { *d };
         new_data_bytes.push(invalid_data);
     }
-
     let new_data_string = String::from_utf8(new_data_bytes.clone()).unwrap();
     let new_data = new_data_string.as_str();
     let mut output = String::new();
-    let res0 = encode_into(new_data.chars(), &mut output);
-    println!("Result: {:?}", res0);
+    encode_into(new_data.chars(), &mut output).unwrap();
+    println!("Result1: {:?}, modified input: {:?}", output, new_data);
 
+/*
+    // test 2
     if let Ok(domain) =  std::str::from_utf8(&new_data_bytes) {
-        // Generate a long nested domain by repeating the domain multiple times
-        let long_nested_domain = domain.repeat(100);  // Arbitrary large number
+        let long_nested_domain = domain.repeat(1000);
         let res = domain_to_ascii(&long_nested_domain);
-        println!("Result: {:?}", res);
+        println!("Result2: {:?}", res);
     } else {
         println!("Invalid domain");
     }
-
-    // Additionally, pass malformed inputs (invalid UTF-8 byte sequences) directly
-    // Attempt to convert invalid byte sequences to provoke panics
+ */
+    // test 3
     let malformed_input = unsafe { std::str::from_utf8_unchecked(data_byte) };
-    let res2 = domain_to_ascii(malformed_input);
-    println!("Result2: {:?}", res2);
+    let res3 = domain_to_ascii(malformed_input);
+    println!("Result3:{:?}, modified input {:?}", res3, malformed_input);
+    let res4 = decode(malformed_input);
+    println!("Result4:{:?}", res4);
+    let res5 = decode_to_string(malformed_input);
+    println!("Result5:{:?}", res5);
+    let res6 = encode_str(malformed_input);
+    println!("Result6:{:?}", res6);
     Ok(())
 }
 
 #[test]
 fn quick_test2() {
-    let data = "http://דוגמה.ישראל:8080/login?username=test&auth=fail#retry\n";
+    let data = "`https://@💩.g메롱.똥.집ws    `";
+    // let data = "http://דוגמה.ישראל:8080/login?username=test&auth=fail#retry\n";
     let res = run(data);
     println!("- idna result: {:?}", res);
 }
